5effecca586734c7475e2c8d34d8a816bfd227b9
Akli DIF <difakli049@gmail.com>
2025-02-09 19:46:28 +0100

    Add load_data function

--- page2.py
+++ page2.py
@@ -1,5 +1,14 @@
 import streamlit as st
-
+import pandas as pd
 
 st.title("Page 2")
 st.write("Bienvenue sur la Page 2. Cette page est réservée aux utilisateurs avec le rôle 2.")
+
+def load_data() -> pd.DataFrame | None:
+    uploaded_file = st.file_uploader("Téléchargez votre fichier CSV", type=["csv"])
+    if uploaded_file:
+        df = pd.read_csv(uploaded_file)
+        st.write("Aperçu des données :", df.head())
+        return df
+    return None
+
